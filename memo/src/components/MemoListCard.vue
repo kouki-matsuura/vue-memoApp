b266a6d7a7e1ce81b69133e239b8ccbba8488973
kouki-matsuura <kouki19991222@icloud.com>
2022-06-15 22:48:02 +0900

    メモのリスト詳細

--- memo/src/components/MemoListCard.vue
+++ memo/src/components/MemoListCard.vue
@@ -1,4 +1,31 @@
-<memo-list-card
-  v-for="(memo, index) in memos"     <!-- 1-1 -->
-  v-bind:key="index"
-  v-bind:memo="memo"/>
+<template>
+<div>
+  <router-link
+    v-bind:to="{name: 'MemoDetails', params:{ id: memo.id }}"
+    v-bind:class="{ 'btn-sm btn-primary': !memo.million, 'btn-sm btn-success': memo.million}">
+    {{ memo.id }}
+  </router-link>
+  <span class="card-title">{{ formatedTitle }}</span>
+
+</div>
+</template>
+<script>
+export default {
+  computed: {
+    formatedTitle () {
+      if (!this.memo || !this.memo.title) {
+        return ''
+      }
+      return this.getOmissionAndPlusMidpoint(this.memo.title, 16)
+    }
+  },
+  methods: {
+    getOmissionAndPlusMidpoint (str, limit) {
+      if (str.length < limit) {
+        return str
+      }
+      return str.substr(0, limit) + '...'
+    }
+  }
+}
+</script>
--- memo/src/pages/MemoList.vue
+++ memo/src/pages/MemoList.vue
@@ -1,3 +1,27 @@
+<template>
+    <div>
+        <memo-list-card 
+            v-for="(memo, index) in memos"
+            v-bind:key="index"
+            v-bind:memo="memo">
+        </memo-list-card>
+        <memo-list-form />
+    
+        <button class="btn-sm btn-dark m-1"
+        v-for="(memo, index) in memos"
+        v-bind:key="index"
+        v-on:click="removeMemo(memo.id)">
+        {{ memo.id }}
+        </button>
+
+        <button class="btn-sm btn-light m-1"
+        v-for="(memo, index) in memos"
+        v-bind:key="index"
+        v-on:click="toggleMillion(memo.id)">
+        {{ memo.id }}
+        </button>
+    </div>
+</template>
 <script>
 import MemoListCard from '@/components/MemoListCard'
 import MemoListForm from '@/components/MemoListForm'
@@ -13,6 +37,15 @@ export default {
         memos () {
             return this.$store.getters.memos
         }
-    }
+    },
+
+    methods: {
+        removeMemo (_id) {
+            this.$store.commit('removeMemo', {id: parseInt(_id, 10)})
+        },
+        toggleMillion (_id) {
+            this.$store.commit('toggleMillion', {id: parseInt(_id, 10)})
+        }
+    },
 }
 </script>
