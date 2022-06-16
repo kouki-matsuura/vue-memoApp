4d40e684727d162147a7e8540b8bd4d574d0b20e
松浦　昂生 <kouki19991222@icloud.com>
2022-06-16 21:04:39 +0900

    [add] 必要なファイルの追加

--- memo/src/components/MemoListCard.vue
+++ memo/src/components/MemoListCard.vue
@@ -1,31 +1,61 @@
 <template>
-<div>
-  <router-link
-    v-bind:to="{name: 'MemoDetails', params:{ id: memo.id }}"
-    v-bind:class="{ 'btn-sm btn-primary': !memo.million, 'btn-sm btn-success': memo.million}">
-    {{ memo.id }}
-  </router-link>
-  <span class="card-title">{{ formatedTitle }}</span>
+<div class="card p-2 mb-2 float-lg-left" style="width: 18rem;" v-bind:class="{ 'border-primary': !memo.million, 'border-success': memo.million}">
+
+  
 
 </div>
 </template>
 <script>
 export default {
+  name: 'MemoListCard',
+  data() {
+    return {
+      releasedAtFromNow: this.getReleasedAtFromNow()
+    }
+  },
+  mounted() {
+    window.setInterval(() => {
+      this.releasedAtFromNow = this.getReleasedAtFromNow()
+    }, 1000 * 60)
+  },
+  props: {
+    'memo': {
+      type: Object,
+      required: true
+    }
+  },
   computed: {
     formatedTitle () {
       if (!this.memo || !this.memo.title) {
         return ''
       }
       return this.getOmissionAndPlusMidpoint(this.memo.title, 16)
+    },
+    formatedDescription () {
+      if (!this.memo || !this.memo.description) {
+        return ''
+      }
+      return this.getOmissionAndPlusMidpoint(this.memo.description, 60)
+    }
+  },
+  watch: {
+    'memo' (n, o) {
+      console.log('watch memo');
     }
   },
   methods: {
+    getReleasedAtFromNow () {
+      if (!this.memo || !this.memo.releasedAt) {
+        return ''
+      }
+      return this.$moment(this.memo.releasedAt).fromNow();
+    },
     getOmissionAndPlusMidpoint (str, limit) {
       if (str.length < limit) {
         return str
       }
       return str.substr(0, limit) + '...'
     }
-  }
+  },
 }
 </script>
--- memo/src/router/index.js
+++ memo/src/router/index.js
@@ -3,14 +3,23 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MemoList from '@/pages/MemoList'
 import MemoDetails from '@/pages/MemoDetails'
+import HelloWorld from '@/pages/HelloWorld'
+import SimpleForm from '@/pages/SimpleForm'
+import GridView from '@/pages/GridView'
+import PopperView from '@/pages/PopperView'
+import CheckOut from '@/pages/CheckOut'
+import SignIn from '@/pages/SignIn'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'HelloWorld',
+    component: HelloWorld,
+    meta : {
+      title: 'hello world'
+    }
   },
   {
     path: '/about',
@@ -36,11 +45,79 @@ const routes = [
     meta: {
       title: 'details of memo'
     }
+  },
+  {
+    path: '/sign-in',
+    name: 'SignIn',
+    component: SignIn,
+    meta: {
+      title: 'sign in'
+    }
+  },
+  {
+    path: '/simple-form',
+    name: 'SimpleForm',
+    component: SimpleForm,
+    meta: {
+      title: 'simple form'
+    }
+  },
+  {
+    path: '/grid-view',
+    name: 'GridView',
+    component: GridView,
+    meta: {
+      title: 'grid view'
+    }
+  },
+  {
+    path: '/popper-view',
+    name: 'PopperView',
+    component: PopperView,
+    meta: {
+      title: 'popper view'
+    }
+  },
+  {
+    path: '/check-out',
+    name: 'CheckOut',
+    component: CheckOut,
+    meta: {
+      title: 'check out'
+    }
+  },
+  {
+    path: '*',
+    redirect :{
+      name: 'HelloWorld'
+    }
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior: (to, from, savedPosition) => {
+    return savedPosition || {x: 0, y:0}
+  }
+})
+
+//グローバル　beforeガード
+router.beforeEach((to, from, next) => {
+  //to: route:　次にナビゲーションされる対象のルートオブジェクト
+  //from: route: ナビゲーションされる前の現在のルート
+  //next: function: フックを解決するためにこの関数を呼ぶ必要がある
+  next()
+})
+
+//グローバル afterガード
+router.afterEach((to, from) => {
+  if (to.meta && to.meta.title) {
+    if (to.params && to.params.id) {
+      document.title = `${to.meta.title} # ${to.params.id}`
+    }else {
+      document.title = to.meta.title
+    }
+  }
 })
 
 export default router
